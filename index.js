c16626c081eaeaf4757f866a0da89e62086cf93f
Burrichen <155198717+Burrichen@users.noreply.github.com>
2025-11-05 15:39:04 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -7,5 +7,15 @@ const img2 = PNG.sync.read(fs.readFileSync('img2.png'));
 const { width, height } = img1;
 const diff = new PNG({ width, height });
 
+
+function getDateString() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const day =`${date.getDate()}`.padStart(2, '0');
+  return `${day}${month}${year}`
+}
+
 pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.50 });
-fs.writeFileSync('diff.png', PNG.sync.write(diff));
+fs.writeFileSync(`diff_${getDateString()}.png`, PNG.sync.write(diff));
+
